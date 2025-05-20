51fb9a9980741153ce831ee2adf556f223ade84c
adrian <adri.ferter2@gmail.com>
2025-05-20 09:20:15 +0000

    Boton de Iniciar sin cuenta

--- index.php
+++ index.php
@@ -7,8 +7,9 @@ include ("header.php");
   echo "<div class='container mt-5'>";
   echo "<h1>Bienvendio/a a Letterfly</h1>";
   echo "<p>Únete a nosotros</p>";
-  echo "<a href='login.php' class='btn btn-lg hola'>Iniciar sesión</a>";
-  echo "<a href='registro.php' class='btn btn-dark btn-lg text-white'>Regístrate</a>";
+  echo "<a href='login.php' class='btn btn-lg hola ms-2'>Iniciar sesión</a>";
+  echo "<a href='registro.php' class='btn btn-dark btn-lg text-white ms-2'>Regístrate</a>";
+  echo "<a href='libros.php' class='btn btn-secondary btn-lg ms-2'>Entrar Sin Cuenta</a>";
   echo "</div>";
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
